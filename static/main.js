179c1f89e69a11f7972395158e7bc87a202f7c17
Jegardo <panteliskavv@gmail.com>
2021-04-13 16:41:06 +0300

    angular poller and stuff

--- static/main.js
+++ static/main.js
@@ -1,23 +1,49 @@
-(fucntion () {
+(function () {
+
   'use strict';
 
   angular.module('WordcountApp', [])
 
-  .controller('WordcountController', ['$scope', '$log', '$http'
-    function($scope, $log, $http) {
+  .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
+    function($scope, $log, $http, $timeout) {
       $scope.getResults = function() {
-        $log.log("tastta");
-
+        $log.log("test");
+        // get the URL from the input
         var userInput = $scope.url;
-
+        //fire the API request
         $http.post('/start', {"url": userInput}).
           success(function(results) {
             $log.log(results);
+            getWordCount(results);
           }).
           error(function(error) {
             $log.log(error);
           });
       };
+
+      function getWordCount(jobID) {
+        var timeout = "";
+
+        var poller = function() {
+          // fire another request
+          $http.get('/results/'+jobID).
+            success(function(data, status, headers, config) {
+              if(status === 202) {
+                $log.log(data, status);
+              } else if (status === 200) {
+                $log.log(data);
+                $scope.wordcounts = data;
+                $timeout.cancel(timeout);
+                return false;
+              }
+              // countinue to call the poller() function every 2 secs
+              // until the  timeout is cancelled
+              timeout = $timeout(poller, 2000);
+            });
+        };
+        poller();
+      }
+
     }
   ]);
 
